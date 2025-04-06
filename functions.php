4ff0609fb3f4fcbfa94918e1262f2cab413e72df
ivan5686 <ivan5686@hotmail.com>
2025-04-07 01:59:20 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 
 create_function('get_header', function() {
-    // Aquí puedes incluir el código para mostrar el encabezado de tu sitio
+   
     echo '<header><h1>ProyectoSem3</h1></header>';
 });
 <?php
-get_header(); // Asegúrate de que este archivo existe y contiene la función get_header()
+get_header(); 
 if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 
